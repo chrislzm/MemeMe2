2c776ca3097cd2a7b78d859da1a624f89acfad3b
chrislzm <me@chrisleung.com>
2017-03-22 20:54:40 -0700

    Implemented keyboard will hide functionality

--- MemeMe1/ViewController.swift
+++ MemeMe1/ViewController.swift
@@ -96,6 +96,7 @@ class ViewController: UIViewController,  UIImagePickerControllerDelegate, UINavi
     func subscribeToKeyboardNotifications() {
         
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
     }
     
     func unsubscribeFromKeyboardNotifications() {
@@ -108,8 +109,13 @@ class ViewController: UIViewController,  UIImagePickerControllerDelegate, UINavi
         view.frame.origin.y -= getKeyboardHeight(notification)
     }
     
-    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+    func keyboardWillHide(_ notification:Notification) {
         
+        view.frame.origin.y = 0
+    }
+
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
         let userInfo = notification.userInfo
         let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
         return keyboardSize.cgRectValue.height
