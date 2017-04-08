9d68026a4020d45142097972e3cdc62d2a1bc553
chrislzm <me@chrisleung.com>
2017-04-07 23:02:30 -0700

    Updated comments for readability

--- MemeMe2/Extensions.swift
+++ MemeMe2/Extensions.swift
@@ -1,7 +1,6 @@
 //
-//  ProtocolsAndExtensions.swift
+//  Extensions.swift
 //  For Meme v2.0 Project
-//  Methods that are either used by multiple classes or related to those used by multiple classes -- consolidated here in order to centralize related code and eliminate duplicate code.
 //
 //  Created by Chris Leung on 3/30/17.
 //  Copyright © 2017 Chris Leung. All rights reserved.
@@ -9,16 +8,17 @@
 
 import UIKit
 
+// MARK: UIViewController extension
 
 extension UIViewController {
 
-    // MARK: Returns the current saved memes array
+    // Returns the current saved memes array
     func getMemes() -> [Meme] {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.memes
     }
 
-    // MARK: Set meme label font attributes (for cells in SentMemeTableViewController and SentMemeCollectionViewController)
+    // Sets meme label font attributes (for cells in SentMemeTableViewController and SentMemeCollectionViewController)
     func setupMemeLabelAttributes(_ label:UILabel,_ text:String) {
         
         // Modify parameters both here and in sharedMemeTextAttributesWith(_:,_:) if you want to change text style/attributes
@@ -34,7 +34,7 @@ extension UIViewController {
         label.backgroundColor = backgroundColor
     }
     
-    // MARK: Set text field font attributes (for editable text fields in EditMemeViewController). Implemented here since it uses text attributes that are shared with other parts of the application.
+    // Sets text field font attributes (for editable text fields in EditMemeViewController). Implemented here since it uses text attributes that are shared with other parts of the application.
     func setupMemeTextFieldAttributes(_ textField:UITextField, _ delegate:UITextFieldDelegate) {
         
         // Modify parameters both here and in sharedMemeTextAttributesWith(_:,_:) if you want to change text style/attributes
@@ -51,7 +51,7 @@ extension UIViewController {
         textField.backgroundColor = backgroundColor
     }
     
-    // MARK: Shared meme text attributes. These properties configure meme text in EditMemeViewController, SentMemeTableViewController, and SentMemeCollectionViewController. Font size and stroke width are passed in as arguments.
+    // Returns a dictionary of meme text attributes. These common properties are shared between EditMemeViewController, SentMemeTableViewController, and SentMemeCollectionViewController. Font size and stroke width are passed in as arguments.
     func sharedMemeTextAttributesWith(_ fontSize:CGFloat, _ fontStrokeWidth:CGFloat) -> [String:Any] {
         return [NSStrokeColorAttributeName: UIColor.black,
                 NSForegroundColorAttributeName: UIColor.white,
@@ -60,9 +60,12 @@ extension UIViewController {
     }
 }
 
-// MARK: Code to disable status bar in UIImagePickerControllers (Album and Camera)
+// MARK: UIImagePickerController extension
 
 extension UIImagePickerController {
+    
+    // These two methods disable the status bar while in the album and camera views
+    
     open override var childViewControllerForStatusBarHidden: UIViewController? {
         return nil
     }
